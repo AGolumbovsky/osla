2b95af6d464ce014b16cbd19c0f278f86c0997db
agolumbovsky <agolumbovsky@gmail.com>
2016-11-02 20:10:12 -0400

    stitching the dom

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,15 @@
-function test () {
+function demoer () {
 
-	alert("something")
+	var word = document.getElementById("word").value;
+
+	var partSp = document.getElementById("partSp").value;
+
+	console.log("word is: " + word);
+	console.log("word type of: " + partSp);
+
+	// alert("the word is ", word , "and it is a", partSp);
+
+	var textArea = document.getElementById("text-area");
+	textArea.value = word + " " + partSp;
+
 }
