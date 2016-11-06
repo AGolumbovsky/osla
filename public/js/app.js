966ac7df3fe8da7060b32940e653f679e327ebee
agolumbovsky <agolumbovsky@gmail.com>
2016-11-05 22:06:43 -0400

    test transformation logic

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,4 @@
-$(document).ready(function() {
-
-	$("#word").
-
-})
-
-
-
-
-
-
-
-
+/*
 
 function grammar() {
 
@@ -28,10 +16,55 @@ function grammar() {
 
 }
 
+*/ 
 
 
 
-	var textArea = document.getElementById("text-area");
-	textArea.value = word + "\n" + partSp;
+function transform() {
+
+	var word = document.getElementById("word").value;
+
+	var lastLetter = word.charAt(word.length-1);
+
+	var lastTwoLetters = word.substr(word.length-2);
+
+	var partSp = document.getElementById("partSp").value;
+
+	var content = document.getElementById("text-area").value;
+
+	if(partSp === "noun") {
+
+		if(lastLetter === "a") {
+
+			content = word;
+
+		}
+
+		else {
+
+			content = word + "a";
+
+		}
+
+	}
+
+	else if(partSp === "verb") {
+
+		if(lastTwoLetters === "al") {
+
+			content = word;
+
+		}
+
+		else {
+
+			content = word + "al";
+
+		}
+
+	}
 
 
+	return content;
+
+}
