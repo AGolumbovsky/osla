70edb1590188440ee4740ef71630712fe73f05a1
agolumbovsky <agolumbovsky@gmail.com>
2016-12-19 21:14:22 -0500

    boilerplating reactjs

---
+++ app/components/Main.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+// var Nav = require('Nav');
+
+var Main = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h2>Main Componento</h2>
+            </div>
+        ) 
+    }
+});
+
+module.exports = Main;
