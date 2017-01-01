a61b4a03bf0b6e4f0bafa0df143cd242fac66d34
agolumbovsky <agolumbovsky@gmail.com>
2016-12-31 21:54:25 -0500

    monor PORT changes

---
+++ app/components/AddWord.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var AddWord = (props) => {
+    return (
+        <div>
+            <h3>--Add Word Component-- </h3>
+        </div>
+    )
+}
+
+module.exports = AddWord;
