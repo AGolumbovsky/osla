e5cdd49d8d49dd73c31135c2e38b9376ce784b1d
agolumbovsky <agolumbovsky@gmail.com>
2017-01-11 17:45:46 -0500

    catch up

--- app/components/DictionaryForm.jsx
+++ app/components/DictionaryForm.jsx
@@ -3,13 +3,13 @@ var React = require('react');
 var DictionaryForm = React.createClass ({
     onFormSubmit: function(evt) {
         evt.preventDefault();
-        alert(this.refs.word.value);
 
         var word = this.refs.word.value;
+        console.log(word);
 
         if(word.length > 0) {
             this.refs.word.value = "";
-            this.props.onSearch(word);
+           // this.props.onSearch(word);
         }
     },
     render: function() {
