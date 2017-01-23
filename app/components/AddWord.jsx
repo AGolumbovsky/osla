adce29895b5f318f53702a1c1fef116829489754
agolumbovsky <agolumbovsky@gmail.com>
2017-01-22 20:41:50 -0500

    added test, updated json

--- app/components/AddWord.jsx
+++ app/components/AddWord.jsx
@@ -4,7 +4,7 @@ var AddWord = (props) => {
     return (
         <div>
             <h3>--Add Word Component-- </h3>
-            <h4> props.onSearch: { props.onSearch } </h4>
+           
         </div>
     )
 }
