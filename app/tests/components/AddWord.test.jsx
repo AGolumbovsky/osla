2ea119b9ff814cf45c96ebd18c79dc53a8c77dcc
agolumbovsky <agolumbovsky@gmail.com>
2017-01-22 21:08:35 -0500

    added carma.conf.js

---
+++ app/tests/components/AddWord.test.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var AddWord = require('AddWord');
+
+decribe('AddWord', () => {
+    it('should exist', () => {
+        expect(Clock).toExist();
+    })
+})
