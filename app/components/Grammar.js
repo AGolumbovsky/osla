7d6fd9dfe0d10c1b651688505a3633ffc12866a3
agolumbovsky <agolumbovsky@gmail.com>
2017-02-20 21:36:36 -0500

    jsx to js + redux folders

---
+++ app/components/Grammar.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Grammar = () => {
+	return (
+		<div> 
+			<h6>Give me a moment to figure it out </h6>
+			<article>
+				I don't know but I've been told, polozhil na vse ya bolt
+			</article>
+		</div>
+	);
+};
+module.exports = Grammar;
--- app/components/Grammar.jsx
+++
@@ -1,8 +0,0 @@
-var React = require('react');
-
-var Grammar = () => {
-	return (
-		<div> <h6>Give me a moment to figure it out </h6></div>
-	);
-};
-module.exports = Grammar;
