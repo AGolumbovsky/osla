3cea8d629a5b7ce92cf0aedbf22f8889422ec1b4
agolumbovsky <agolumbovsky@gmail.com>
2017-04-12 11:16:44 -0400

    db conn str for heroku and more form stuff

--- app/components/Details.js
+++ app/components/Details.js
@@ -6,15 +6,15 @@ export class Details extends Component {
 	renderDetails() {
 		var rec = this.props.rec;
 
-		console.log("value of props in Details:", this.props)
+		console.log("value of this.props in Details:", this.props)
 		console.log("value of rec in Details:", rec) // this renders too many times
 
 		return (
 			<div>
 				
-				<h5>id: { rec._id } </h5>
-				<h3>Word: {rec.word} </h3>
-				<h6>Description: {rec.description}</h6>
+				<h2>{rec.word} </h2>
+				<h3> {rec.partSp || "Unknown"} </h3>
+				<h6> {rec.description} </h6>
 				
 			</div>
 		);
