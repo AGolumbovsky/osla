36f9a3839bf162c5553785c94d1e46c211ea458b
agolumbovsky <agolumbovsky@gmail.com>
2017-06-05 19:42:37 -0400

    started on word transformation: actions

---
+++ app/actions/transformers.js
@@ -0,0 +1,21 @@
+import * as c from 'actionTypes';
+
+export const transformWord = (w) => {
+    if (w.partSp == 'noun') 
+}
+
+export const transNoun = (w) => {
+
+}
+
+export const transVerb = (w) => {
+
+}
+
+export const transAdjective = (w) => {
+
+}
+
+export const transAdverb = (w) => {
+
+}
