29fc615340982ea2aff654b8ec73e3809772d7c8
agolumbovsky <agolumbovsky@gmail.com>
2017-08-12 10:10:33 -0400

    more with transformer actions

--- app/actions/transformers.js
+++ app/actions/transformers.js
@@ -1,25 +1,29 @@
 import * as c from 'actionTypes';
 
-export const transformWord = (w) => {
-    if (w.partSp == 'noun') {
-
-    }
-}
-
 export const transformNoun = (w) => {
     return {
-
+        type: c.WORD_IS_NOUN,
+        w
     }
 }
 
 export const transformVerb = (w) => {
-
+    return {
+        type: c.WORD_IS_VERB,
+        w
+    }
 }
 
 export const transformAdjective = (w) => {
-
+    return {
+        type: c.WORD_IS_ADJECTIVE,
+        w
+    }
 }
 
 export const transformAdverb = (w) => {
-
+    return {
+        type: c.WORD_IS_ADVERB,
+        w
+    }
 }
